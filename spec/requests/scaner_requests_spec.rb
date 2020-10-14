73f132a342f9679dcecad3a195e6e93d74a7a69b
D.C <example@example.com>
2020-10-14 21:39:21 +0200

    rubocop fixes

--- spec/requests/scaner_requests_spec.rb
+++ spec/requests/scaner_requests_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'Scaner requests', :type => :request do
+RSpec.describe 'Scaner requests', type: :request do
   it '/scan/:barcode' do
     get '/scan/12345678'
     expect(response).to have_http_status(:ok)
