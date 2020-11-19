dc67d53f18a219ce731001976bb4d5f012f4fd87
D.C <example@example.com>
2020-11-19 22:47:50 +0100

    workflow test 1

--- spec/models/db_cache/shopping_spec.rb
+++ spec/models/db_cache/shopping_spec.rb
@@ -3,6 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe DbCache::Shopping, type: :model do
-  # trivial ommited
+  # trivial tests ommited
   pending "add some examples to (or delete) #{__FILE__}"
 end
